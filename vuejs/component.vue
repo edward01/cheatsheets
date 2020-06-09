24e45563a0df5132d896e863fc0fa7d231377ddb
Edward Jose <edward@me.com>
2020-06-09 15:45:59 +0800

    [vuejs] get path using component name

---
+++ vuejs/component.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  getComponentPath(componentName) {
+    const link = this.$router.resolve({
+      name: componentName,
+    })
+    console.log('link:', link)
+    return link.route.path
+  }
+}
+</script>
